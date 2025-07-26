27df2a59985341a41609e8158b66aab51e710912
iLLRaps3 <140134684+iLLRaps3@users.noreply.github.com>
2025-07-25 23:42:54 -0700

    Add .next/static/chunks/src_app_ai-assistant_page_tsx_c0237562._.js 1753512174771

---
+++ .next/static/chunks/src_app_ai-assistant_page_tsx_c0237562._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_c57f7421._.js"
+],
+    source: "dynamic"
+});
